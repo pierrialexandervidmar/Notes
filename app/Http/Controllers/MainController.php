39dc4c92348408f6112293dae4245e4c7b1a061d
pierrialexandervidmar <pierri@hotmail.com.br>
2024-11-24 16:03:08 -0300

    Ajustes no layout

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,11 +6,16 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-  public function index() {
-    echo 'Página interna do sistema, home';
+  public function index() 
+  {
+    // Load Users Notes
+
+    // show home view
+    return view('home');
   }
 
-  public function newNote() {
+  public function newNote() 
+  {
     echo 'Formulário para criação de uma nova nota';
   }
 }
